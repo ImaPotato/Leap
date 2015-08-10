2b9a6e6447d7870372060894859257d373e28a43
AshBlankets <ashleighak@hotmail.com>
2015-08-11 10:13:01 +1200

    rotating and 'scaling'
    rotating works. circle gesture in edit mode. swiping to change mode is a
    bit awkward so added screentap as mode change for now.
    two hands to scale in edit mode. sometimes only shows up with
    simultaneous circle gesture

--- js/draw.js
+++ js/draw.js
@@ -1,6 +1,10 @@
 var draw = function(){
 
   var selectedImage = '';
+  var mode = 1; //1 is regular and -1 is edit mode
+
+var x;
+var y;
 
   function concatData(id, data) {
       return id + ": " + data + "<br>";
@@ -15,13 +19,18 @@ var draw = function(){
     for (var i = 0; i < hand.length; i++) { //for each of the hands
     
       var finger = hand[i].fingers[1]; //index finger
-      
       var pos = finger.dipPosition;
 
       // create a circle for the finger
       var radius = 50;
-      var x = pos[0]*5;
-      var y = $(window).height() - pos[1]*3.5;
+
+    //previous x, y
+    // var x = pos[0]*5;
+    //var y = $(window).height() - pos[1]*3.5;
+
+    //hand directly above Leap should be in middle of screen
+    x = $(window).width()/2 + pos[0]*6 ;
+    y = $(window).height() - pos[1]*3;
 
       //  update the cursor
       $("#hand" + i).css({
@@ -30,8 +39,8 @@ var draw = function(){
         "left": x + "px",
       });
 
-      // if we've selected an image move it to its new position
-      if(selectedImage != ''){
+      // if we've selected an image and we're in moving mode move it to its new position
+      if(selectedImage != '' && mode == 1){
         $('#' + selectedImage).css({
           "position": "absolute", 
           "top": y + "px",
@@ -39,7 +48,7 @@ var draw = function(){
         });
       }
 
-      if(hand[i].pinchStrength > 0.9){
+      if(hand[i].pinchStrength > 0.85){
         if(selectedImage == '') onPicture(x, y);
       } 
 
@@ -55,12 +64,13 @@ var draw = function(){
             //hand the gesture occured on
 
           });
-
+          var m = mode==1 ? "moving mode  " : "edit mode  ";
           switch (gesture.type){
             case "circle":
                 console.log("Circle Gesture");
-                //we will probably want to improve this as well
-                if(selectedImage != ''){
+                document.getElementById("output").innerHTML = m+ " circle gesture "+ selectedImage;
+                //if we're in edit mode -rotate
+                if(selectedImage != '' && mode == -1){
 
                   var clockwise = false;
                   var pointableID = gesture.pointableIds[0];
@@ -68,26 +78,50 @@ var draw = function(){
                   var dotProduct = Leap.vec3.dot(direction, gesture.normal);
 
                   if (dotProduct  >  0) clockwise = true;
-                  if (!clockwise) increaseSize(); else decreaseSize();
+                  if (!clockwise) rotateACW(); else rotateCW();
 
-                } 
-
+                }
                 break;
             case "keyTap":
                 console.log("Key Tap Gesture");
+                document.getElementById("output").innerHTML = m + " keytap gesture";
                 // this is pretty gross as well but if we've got something selected and we do this gesture we will put it back down again
                 if(selectedImage != '') selectedImage = '';
                 break;
             case "screenTap":
+            //tying screenTap to mode change because swipe is awful
                 console.log("Screen Tap Gesture");
+                document.getElementById("output").innerHTML = m+" screenTap gesture";
+                //edit mode
+                mode = mode *-1;
                 break;
             case "swipe":
                 console.log("Swipe Gesture");
+                document.getElementById("output").innerHTML = m+" swipe gesture";
+                //edit mode
+                mode = mode *-1;
                 break;
             }
+
+
         });
+        //both hands on screen and edit mode
+            if(frame.hands.length==2 && selectedImage != '' && mode==-1){
+             document.getElementById("output").innerHTML = " scaling "+ selectedImage;
+
+              //scaling
+              var x1 = hand[0].fingers[1].dipPosition[0];
+              var y1 = hand[0].fingers[1].dipPosition[1];
+
+              var x2 = hand[1].fingers[1].dipPosition[0];
+              var y2 = hand[1].fingers[1].dipPosition[1];
+                
+              var dist = Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );  
+              if (dist> 100) increaseSize(); else decreaseSize();
+            }
       }
-    }
+    } 
+         
 
      //printing some data
     var output = document.getElementById('output');
@@ -123,6 +157,18 @@ var draw = function(){
   }
   function decreaseSize(){
     $('#' + selectedImage).css({ "width": "-=" + $('#' + selectedImage).width() * .01, "height": "-=" + $('#' + selectedImage).height() * .01});
+  }
+
+  function rotateACW(){
+    var img = $('#' + selectedImage);
+    var angle = img.getRotateAngle();
+    img.rotate(angle -0.5);
+  }
+
+  function rotateCW(){
+    var img = $('#' + selectedImage);
+    var angle = img.getRotateAngle();
+    img.rotate(angle -359.5);
   }
 
   // listen to Leap Motion
